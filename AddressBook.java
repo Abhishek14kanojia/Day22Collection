f622e0fc4bafce1fab4eafe9ddcc6ae1ea2b0c31
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-11 13:38:51 +0530

    AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -5,84 +5,99 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-	 private HashMap<String, Contacts> contacts;
-	    private static Scanner scanner = new Scanner(System.in);;
-
+	 private Contacts[] contacts;
+	    private static Scanner scanner = new Scanner(System.in);
+	    private int noOfContacts;
 	    public AddressBook() {
-	        this.contacts = new HashMap<String, Contacts>();
+	        this.contacts = new Contacts[10];
+	        this.noOfContacts = 0;
 	    }
 	    public void editContact() {
-	        System.out.println("Enter first name of person you want edit:");
-	        String firstName = scanner.nextLine();
-	        Contacts contact = contacts.get(firstName.toLowerCase());
-	        System.out.println("Select Option:\n1. First Name\n2. Last Name\n 3. City\n4. State\n5. Zip\n6. Phone\n7. Email");
+	        System.out.println("Enter phone of person you want edit:");
+	        long phone = scanner.nextLong();
+	        Contacts contactToEdit = null;
+	        for(Contacts contact : contacts) {
+	            if(contact.getPhoneNumber() == (phone)) {
+	                contactToEdit = contact;
+	                break;
+	            }
+	        }
+	        System.out
+	                .println("Select Option:\n1. First Name\n2. Last Name\n 3. City\n4. State\n5. Zip\n6. Phone\n7. Email");
 	        int choice = Integer.parseInt(scanner.nextLine());
 
 	        switch (choice) {
 	            case 1:
 	                System.out.println("Enter new first name:");
-	                contacts.remove(firstName);
 	                String newName = scanner.nextLine();
-	                contact.setFirstName(newName);
-	                contacts.put(newName, contact);
-	                System.out.println(contact.getFirstName());
+	                contactToEdit.setFirstName(newName);
 	                break;
 
 	            case 2:
 	                System.out.println("Enter new last name:");
-	                contact.setLastName(scanner.nextLine());
+	                contactToEdit.setLastName(scanner.nextLine());
 	                break;
 
 	            case 3:
 	                System.out.println("Enter new city");
-	                contact.setCity(scanner.nextLine());
+	                contactToEdit.setCity(scanner.nextLine());
 	                break;
 
 	            case 4:
 	                System.out.println("Enter new State");
-	                contact.setState(scanner.nextLine());
+	                contactToEdit.setState(scanner.nextLine());
 	                break;
 
 	            case 5:
 	                System.out.println("Enter new Zip");
-	                contact.setZip(scanner.nextInt());
+	                contactToEdit.setZip(scanner.nextInt());
 	                break;
 
 	            case 6:
 	                System.out.println("Enter new Phone");
-	                contact.setPhoneNumber(scanner.nextLong());
+	                contactToEdit.setPhoneNumber(scanner.nextLong());
 	                break;
 
 	            case 7:
 	                System.out.println("Enter new email");
-	                contact.setEmail(scanner.nextLine());
+	                contactToEdit.setEmail(scanner.nextLine());
 	                break;
 
 	            default:
 	                System.err.println("Invalid Option");
 	        }
 	    }
- public void addContact() {
-     System.out.println("Enter the contact :");
-     System.out.println("Enter first name:");
-     String firstName = scanner.nextLine();
-     System.out.println("Enter last name");
-     String lastName = scanner.nextLine();
-     System.out.println("Enter city");
-     String city = scanner.nextLine();
-     System.out.println("Enter state");
-     String state = scanner.nextLine();
-     System.out.println("Enter Zip");
-     int zip = scanner.nextInt();
-     System.out.println("Enter Phone");
-     long phoneNumber = scanner.nextLong();
-     System.out.println("Enter email");
-     String email = scanner.nextLine();
+	    public void deleteContact() {
+	        System.out.println("Enter phone number of person you want to delete:");
+	        long phone = scanner.nextLong();
+	        int index=0;
+	        for(;index<noOfContacts;index++) {
+	            if(contacts[index].getPhoneNumber() == (phone)) {
+	                break;
+	            }
+	        }
+	        for(;index<noOfContacts-1;index++) {
+	            contacts[index] = contacts[index+1];
+	        }
+	        System.out.println("Successfully Deleted");
+	    }
+	    public void addContact() {
+	        System.out.println("Enter the contact :");
+	        System.out.println("Enter first name:");
+	        String firstName = scanner.nextLine();
+	        System.out.println("Enter last name");
+	        String lastName = scanner.nextLine();
+	        System.out.println("Enter city");
+	        String city = scanner.nextLine();
+	        System.out.println("Enter state");
+	        String state = scanner.nextLine();
+	        System.out.println("Enter Zip");
+	        int zip = scanner.nextInt();
+	        System.out.println("Enter Phone");
+	        long phoneNumber = scanner.nextLong();
+	        System.out.println("Enter email");
+	        String email = scanner.nextLine();
 
-     contacts.put(firstName.toLowerCase(),(new Contacts(firstName, lastName, city, state, zip, phoneNumber, email)));
- }
- public void deleteContact() {
-    String firstName = scanner.nextLine();
-     contacts.remove(firstName.toLowerCase());
- }
+	        contacts[noOfContacts++] = new Contacts(firstName, lastName, city, state, zip, phoneNumber, email);
+	    }
 }
