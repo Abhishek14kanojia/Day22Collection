8be9eb8e155af36fa925ff2636bbf413bd4607ce
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-11 13:23:13 +0530

    AddressBook

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.day22Collection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Contact contact = new Contact("Abhishek", "Kanojia", "Ujjain", "MadhyaPradesh", 456331, 8989232830L, "abc@gmail.com");
+		System.out.println(contact);
+	}
+
+}
