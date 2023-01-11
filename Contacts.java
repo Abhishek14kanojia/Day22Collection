f1061d2303ad71b040158f610e05668f151f390b
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-11 13:41:22 +0530

    AddressBook

--- Contacts.java
+++ Contacts.java
@@ -3,78 +3,79 @@ package com.day22Collection;
 public class Contacts {
 
 	
-		private  String firstName;
-	    private  String lastName;
-	    private  String city;
-	    private  String state;
-	    private  int zip;
-	    private  long phoneNumber;
-	    private  String email;
+	private  String firstName;
+    private  String lastName;
+    private  String city;
+    private  String state;
+    private  int zip;
+    private  long phoneNumber;
+    private  String email;
 
-	    public Contacts(String firstName, String lastName, String city, String state, int zip, long phoneNumber, String email) {
-	        this.firstName = firstName;
-	        this.lastName = lastName;
-	        this.city = city;
-	        this.state = state;
-	        this.zip = zip;
-	        this.phoneNumber = phoneNumber;
-	        this.email = email;
-	    }
+    public Contacts(String firstName, String lastName, String city, String state, int zip, long phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 
-		public final String getFirstName() {
-			return firstName;
-		}
+	public final String getFirstName() {
+		return firstName;
+	}
 
-		public final void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
+	public final void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-		public final String getLastName() {
-			return lastName;
-		}
+	public final String getLastName() {
+		return lastName;
+	}
 
-		public final void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
+	public final void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-		public final String getCity() {
-			return city;
-		}
+	public final String getCity() {
+		return city;
+	}
 
-		public final void setCity(String city) {
-			this.city = city;
-		}
+	public final void setCity(String city) {
+		this.city = city;
+	}
 
-		public final String getState() {
-			return state;
-		}
+	public final String getState() {
+		return state;
+	}
 
-		public final void setState(String state) {
-			this.state = state;
-		}
+	public final void setState(String state) {
+		this.state = state;
+	}
 
-		public final int getZip() {
-			return zip;
-		}
+	public final int getZip() {
+		return zip;
+	}
 
-		public final void setZip(int zip) {
-			this.zip = zip;
-		}
+	public final void setZip(int zip) {
+		this.zip = zip;
+	}
 
-		public final long getPhoneNumber() {
-			return phoneNumber;
-		}
+	public final long getPhoneNumber() {
+		return phoneNumber;
+	}
 
-		public final void setPhoneNumber(long phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}
+	public final void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-		public final String getEmail() {
-			return email;
-		}
+	public final String getEmail() {
+		return email;
+	}
 
-		public final void setEmail(String email) {
-			this.email = email;
-		}
+	public final void setEmail(String email) {
+		this.email = email;
+	}
+
 
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,54 @@
 package com.day22Collection;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		AddressBook addressbook = new AddressBook();
-		addressbook.addContact();
-		addressbook.editContact();
-		addressbook.deleteContact();
-	}
+	 private AddressBook[] addressBooks;
+	    private int noOfAddressBooks;
+
+	    public Main() {
+	        this.addressBooks = new AddressBook[5];
+	        this.noOfAddressBooks = 0;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book Program");
+	        Main addressBookMain = new Main();
+	        addressBookMain.addAddressBook();
+	        addressBookMain.addAddressBook();
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter ID of address book (-1 to exit):");
+	        int bookChoice = Integer.parseInt(scanner.nextLine());
+	        if (bookChoice == -1) {
+	            return;
+	        }
+	        while (true) {
+	            System.out.println("Enter 1. Add,2. Edit, 3. Delete, 4. Exit");
+	            int actionChoice = Integer.parseInt(scanner.nextLine());
+	            switch (actionChoice) {
+	                case 1:
+	                    addressBookMain.addressBooks[bookChoice].addContact();
+	                    break;
+
+	                case 2:
+	                    addressBookMain.addressBooks[bookChoice].editContact();
+	                    break;
+
+	                case 3:
+	                    addressBookMain.addressBooks[bookChoice].deleteContact();
+	                    break;
+
+	                case 4:
+	                    System.exit(0);
+	                    break;
+	            }
+	        }
+	    }
+
+	    public void addAddressBook() {
+	        addressBooks[noOfAddressBooks++] = new AddressBook(noOfAddressBooks);
+	    }
 
 }
  
